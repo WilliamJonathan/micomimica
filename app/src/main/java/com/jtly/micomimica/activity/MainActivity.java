b95ca002d6ba3a38711ed00ff74c641e8f02ab32
Jonathan <jonathanwilliamfirebase@gmail.com>
2019-11-27 13:50:53 -0300

    Jonathan William

--- app/src/main/java/com/jtly/micomimica/activity/MainActivity.java
+++ app/src/main/java/com/jtly/micomimica/activity/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView palavrarandom;
     private Button gerar;
+    private int palavra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +26,63 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void gerarPalavra(View view){
-
-        String[] palavras = {
-                "Sapato","Calça", "Bombeiro", "Brasil",
-                "Parana", "Sexo", "Mauricio Boloia", "Aramis Boloia",
-                "mauricio me traz um balde"
+    public void gerarPalavraRandomica(){
+        String[] profissao = {
+                "Advogado","Bancario", "Desenvolvedor", "Atendente de Telemarketing",
+                "Cantor", "Ator", "Jogardor de futebol", "Professor",
+                "Motorista", "balconista", "Frentista"
         };
 
-        int numero = new Random().nextInt(palavras.length);
-        palavrarandom.setText(palavras[numero]);
+        String[] esporte = {
+                "Futebol", "Golf", "Natação", "Maratonismo",
+                "Automobilismo", "Ciclismo", "Crossfit", "MMA",
+                "Baseball"
+        };
+
+        String[] filme = {
+                "Lagoa azul", "Titanic", "Vingadores", "Esterminador do futuro",
+                "A mulher de preto", "Anabelle", "Godzilla", "Velozes e furiosos",
+                "Bastardos inglórios"
+        };
+
+        String[] objeto = {
+                "Faca", "Tesoura", "Balde de flor", "Garrafa de agua",
+                "Caneta", "Celular", "Mochila", "Poutrona",
+                "Agulha"
+        };
+
+        String[] animal = {
+                "Cavalo", "Leão", "Cachorro", "Coelho",
+                "Canguru", "Coala", "Zebra", "Ornitorrinco",
+                "Baleia Azul"
+        };
+
+        String[][] numRandom = {profissao, esporte, filme, objeto, animal};
+
+        int categoria = new Random().nextInt(numRandom.length);
+
+        switch (categoria){
+            case 0:
+                palavra = new Random().nextInt(profissao.length);
+                break;
+            case 1:
+                palavra = new Random().nextInt(esporte.length);
+                break;
+            case 2:
+                palavra = new Random().nextInt(filme.length);
+                break;
+            case 3:
+                palavra = new Random().nextInt(objeto.length);
+                break;
+            case 4:
+                palavra = new Random().nextInt(animal.length);
+                break;
+        }
+
+        palavrarandom.setText(numRandom[categoria][palavra]);
+    }
+
+    public void gerarPalavra(View view){
+        gerarPalavraRandomica();
     }
 }
