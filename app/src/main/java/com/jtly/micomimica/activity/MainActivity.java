12efd809cf4285281438b06d59329cbbe6156452
Jonathan <jonathanwilliamfirebase@gmail.com>
2019-12-10 10:01:49 -0300

    Jonathan William

--- app/src/main/java/com/jtly/micomimica/activity/MainActivity.java
+++ app/src/main/java/com/jtly/micomimica/activity/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         //palavra = findViewById(R.id.txtPalavra);
         //timer = findViewById(R.id.txtTimer);
